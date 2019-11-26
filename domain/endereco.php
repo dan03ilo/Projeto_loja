a5cbf48e34f0a0ef41d5e5ae5a033a144350dc74
DANILO ROCHA LEMOS OLIVEIRA <danilo.rloliveira@senacsp.edu.br>
2019-11-26 11:37:11 -0300

    Concluído os arquivos endereco.php domain e data endereco

---
+++ domain/endereco.php
@@ -0,0 +1,108 @@
+<?php
+
+class Endereco{
+    public $logradouro;
+    public $numero;
+    public $complemento;
+    public $bairro;
+    public $cep;
+
+
+    public function __construct($db){
+        $this->conexao = $db;
+    }
+
+    /*
+    Função listar para selecionar todos os endereços cadastrados no banco
+     de dados. Essa função retorna uma lista com todos os dados.
+    */
+    public function listar(){
+        #Seleciona todos os campos da tabela usuario
+        $query = "select * from endereco";
+
+        /*
+        Foi criada a variável stmt(Statment -> Sentença) para guardar a preparação da consulta
+        select que será executada posteriomente.
+        */
+        $stmt = $this->conexao->prepare($query);
+
+        #execução da consulta e guarda de dados na variável stmt
+        $stmt->execute();
+
+        #retorna os dados do endereço a camada data.
+        return $stmt;
+    }
+
+
+    /*
+    Função para cadastrar os endereços no banco de dados
+    */
+    public function cadastro(){
+        $query = "insert into usuario set logradouro=:l, numero=:n, complemento=:c, bairro=:b, cep=:e";
+
+        $stmt = $this->conexao->prepare($query);
+
+        /*
+        Foi utilizada 2 funções para tratar os dados que estão vindo do endereço
+        para a api.
+        strip_tags-> trata os dados em seus formatos inteiro , double ou decimal
+        htmlspecialchar -> retira as aspas e os 2 pontos que vem do formato 
+        json para cadastrar em banco.
+        */
+        $this->logradouro = htmlspecialchars(strip_tags($this->logradouro));
+        $this->numero = htmlspecialchars(strip_tags($this->numero));
+        $this->complemento = htmlspecialchars(strip_tags($this->complemento));
+        $this->bairro = htmlspecialchars(strip_tags($this->bairro));
+        $this->cep = htmlspecialchars(strip_tags($this->cep));
+
+        
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }        
+    }
+
+    public function atualizarEndereco(){
+        $query = "update usuario set logradouro=:l, numero=:n, complemento=:c, bairro=:b, cep=:e where id=:i";
+
+        $stmt = $this->conexao->prepare($query);
+
+        $stmt->bindParam(":l",$this->logradouro);
+        $stmt->bindParam(":n",$this->numero);
+        $stmt->bindParam(":c",$this->complemento;
+        $stmt->bindParam(":b",$this->bairro);
+        $stmt->bindParam(":e",$this->cep);
+
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function apagar(){
+        $query = "delete from endereco where id=?";
+
+        $stmt=$this->conexao->prepare($query);
+
+        $stmt->bindParam(1,$this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
+
+?>
+
+
+
+
+
