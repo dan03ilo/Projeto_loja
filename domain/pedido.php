807a8ab40fa98c8cf097c20da454cbbbc496ee81
DANILO ROCHA LEMOS OLIVEIRA <danilo.rloliveira@senacsp.edu.br>
2019-11-29 09:55:10 -0300

    Concluído a estrutura domain pedido

---
+++ domain/pedido.php
@@ -0,0 +1,94 @@
+<?php
+
+class Estoque{
+    public $id_cliente;
+    
+
+    public function __construct($db){
+        $this->conexao = $db;
+    }
+
+    /*
+    Função listar para selecionar todos os estoques cadastrados no banco
+    de dados. Essa função retorna uma listar com todos os dados.
+    */
+    public function listar(){
+        #Selecione todos os campos da tabela estoque 
+        $query = "select * from pedido";
+
+        /*
+        Foi criada a variavel stmt(Statment -> Sentença) para guardar a preparação da consulta
+        select que será executada posteriomente.
+        */
+
+        $stmt = $this -> conexao -> prepare ($query);
+
+        #execução da consulta a guarda de dados na variavel stml
+
+        $stmt->execute();
+
+        #retorna os dados do usuario a camada data.
+        return $stmt;
+    }
+    
+    /*
+    Função para cadastrar os estoques no banco de dados
+    */
+    public function cadastro(){
+        $query = "insert into pedido set id_pedido=:p";
+
+        $stmt = $this->conexao->prepare($query);
+
+        /*
+        Foi utilizado 2 funções para tratar os dados que estão vindo do usuário
+        para o api.
+        strip_tags-> trata os dados em seus formatos inteiro , double ou decimal
+        htmlspecialchar-> retira as aspas e os 2 pontos que vem do formato json
+        para cadastrar em banco.
+        */
+
+        $this->id_produto = htmlspecialchars(strip_tags($this->id_pedido));
+
+        $stmt->bindParam(":p",$this->id_pedido);
+
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;;
+        }
+    }
+
+    public function alterarEstoque(){
+        $query = "update pedido set id_pedido=:p, where id=:i";
+
+        $stmt = $this->conexao->prepare($query);
+
+        $stmt->bindParam(":p",$this->id_pedido);
+        $stmt->bindParam(":i",$this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function apagar(){
+        $query = "delete from pedido where id=?";
+
+        $stmt=$this->conexao->prepare($query);
+
+        $stmt->bindParam(1,$this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    }
+
+?>
